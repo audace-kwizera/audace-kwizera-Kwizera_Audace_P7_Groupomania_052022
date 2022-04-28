c73b002430cd77772550ba3082555eab2a6647f8
audace-kwizera <companyfreshbeauty@gmail.com>
2022-04-28 22:41:39 +0200

    install mysql and server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+/** Appel de express */
+const express = require('express');
+/** Importation de .env*/
+require("dotenv").config();
+/** Installation de app express */
+const app = express();
+/** Création du port serveur */
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello  World'));
+
+app.post('/', (req, res) => res.send('Hello Post World'));
+
+app.all('/ping', (req, res) => res.send(+newDate()));
+
+app.listen(port, () => console.log(`Èxemple App listening on port ${port}!`));
